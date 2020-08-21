corrected raned ranges for averaging in angles

cnt.py
@@ -111,6 +111,10 @@ class CNT:
     xBinNumber = np.floor((xMax-xMin) / const.A_CC)
     yBinNumber = np.floor((yMax-yMin) / const.A_CC)
     zBinNumber = np.floor((zMax-zMin) / const.A_CC)
+    
+    if xBinNumber == 0: xBinNumber = 1
+    if yBinNumber == 0: yBinNumber = 1
+    if zBinNumber == 0: zBinNumber = 1
 
     xBins = np.linspace(xMin, xMax, xBinNumber)
     yBins = np.linspace(yMin, yMax, yBinNumber)
@@ -128,7 +132,7 @@ class CNT:
       index1 = indices[i]
       site1 = sites[i]
 
-      nearbySiteNumbers = np.argwhere(spatial.distance.cdist([index1], indices) <= np.sqrt(2.0) + const.EPS)[:,1].reshape((-1))     
+      nearbySiteNumbers = np.argwhere(spatial.distance.cdist([index1], indices2, lambda u, v: np.amax(np.abs(u-v))) < 2)[:,1].reshape((-1))     
 
       if len(nearbySiteNumbers) == 0: continue
 
@@ -174,6 +178,10 @@ class CNT:
     xBinNumber = np.floor((xMax-xMin) / const.A_CC)
     yBinNumber = np.floor((yMax-yMin) / const.A_CC)
     zBinNumber = np.floor((zMax-zMin) / const.A_CC)
+    
+    if xBinNumber == 0: xBinNumber = 1
+    if yBinNumber == 0: yBinNumber = 1
+    if zBinNumber == 0: zBinNumber = 1
 
     xBins = np.linspace(xMin, xMax, xBinNumber)
     yBins = np.linspace(yMin, yMax, yBinNumber)
@@ -195,7 +203,7 @@ class CNT:
       index1 = indices1[i]
       site1 = cnt1.sites[i]
 
-      nearbySiteNumbers = np.argwhere(spatial.distance.cdist([index1], indices2) <= np.sqrt(2.0) + const.EPS)[:,1].reshape((-1))     
+      nearbySiteNumbers = np.argwhere(spatial.distance.cdist([index1], indices2, lambda u, v: np.amax(np.abs(u-v))) < 2)[:,1].reshape((-1))     
 
       if len(nearbySiteNumbers) == 0: continue
 
@@ -240,6 +248,10 @@ class CNT:
     yBinNumber = np.floor((yMax-yMin) / cutoffDistance)
     zBinNumber = np.floor((zMax-zMin) / cutoffDistance)
 
+    if xBinNumber == 0: xBinNumber = 1
+    if yBinNumber == 0: yBinNumber = 1
+    if zBinNumber == 0: zBinNumber = 1
+
     xBins = np.linspace(xMin, xMax, xBinNumber)
     yBins = np.linspace(yMin, yMax, yBinNumber)
     zBins = np.linspace(zMin, zMax, zBinNumber)
@@ -280,7 +292,7 @@ class CNT:
       index1 = indices1[i]
       site1 = cnt1.sites[i]
 
-      nearbySiteNumbers = np.argwhere(spatial.distance.cdist([index1], indices2) <= np.sqrt(2.0) + const.EPS)[:,1].reshape((-1))     
+      nearbySiteNumbers = np.argwhere(spatial.distance.cdist([index1], indices2, lambda u, v: np.amax(np.abs(u-v))) < 2)[:,1].reshape((-1))     
 
       if len(nearbySiteNumbers) == 0: continue
 

systems.py
@@ -650,11 +650,13 @@ class angledContact(System):
     self.m1 = m1
     self.n2 = n2
     self.m2 = m2
-    self.angle = angle
     self.distance = distance
     self.rot1 = rot1
     self.rot2 = rot2
 
+    angle %= np.pi
+    self.angle = angle
+    
     xAxis = (1.0, 0.0, 0.0)
     axis1 = (1.0, 0.0, 0.0)
     axis2 = (np.cos(angle), np.sin(angle), 0.0)
@@ -672,9 +674,10 @@ class angledContact(System):
 
     surfaceDistance = distance - radius1 - radius2
     cutoffDistance = np.sqrt((const.ALPHA - const.DELTA*np.log(const.COUPLING_CUTOFF))**2 - surfaceDistance**2)
+    
 
-    overlap1 = (radius1 + cutoffDistance)/np.tan(angle) + radius2/np.sin(angle) + cutoffDistance
-    overlap2 = (radius2 + cutoffDistance)/np.tan(angle) + radius1/np.sin(angle) + cutoffDistance
+    overlap1 = np.abs((radius1 + cutoffDistance)/np.tan(angle)) + radius2/np.sin(angle) + cutoffDistance
+    overlap2 = np.abs((radius2 + cutoffDistance)/np.tan(angle)) + radius1/np.sin(angle) + cutoffDistance
 
     overlapCellNumber1 = 2 * np.ceil(overlap1/cntUnitCell1.length).astype('int') + 1
     overlapCellNumber2 = 2 * np.ceil(overlap2/cntUnitCell2.length).astype('int') + 1

test.py
@@ -4,8 +4,18 @@ import numpy as np
 import constants as const
 import systems
 
-radius = 1.5/np.pi*const.A_CC * 6
-distance = 2.0 * radius + 3.2
-system = systems.angledContact(6, 0, 6, 6, 0.01*np.pi, distance, offset1=0.4, offset2=0.8)
 
-system.plotSystem()
+n1 = 6
+m1 = 6
+n2 = 6
+m2 = 6
+
+radius1 = 0.5 * const.A_CC * np.sqrt(3.0*(n1**2 + n1*m1 + m1**2)) / np.pi
+radius2 = 0.5 * const.A_CC * np.sqrt(3.0*(n2**2 + n2*m2 + m2**2)) / np.pi
+
+distance = radius1 + radius2 + 3.13
+angle = 0.15
+
+for angle in np.linspace(0.5*np.pi - 0.1, 0.5*np.pi+0.1, 10):
+  system = systems.angledContact(n1, m1, n2, m2, angle, distance)
+  system.plotSystem()